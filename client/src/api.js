36b7180082e80d69f6f118f8c49959fb63243014
bunbebu <nguyenhongphuc0703@gmail.com>
2025-12-11 09:37:48 +0700

    update new api.js in fe

--- client/src/api.js
+++ client/src/api.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: BASE_URL,
 });
 
-// Add a request interceptor to include the token
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
+ (config) => {
+ const token = localStorage.getItem('token');
+ if (token) {
+config.headers.Authorization = `Bearer ${token}`;
+ }
+ return config; },
+(error) => Promise.reject(error)
 );
 
 export default api;
